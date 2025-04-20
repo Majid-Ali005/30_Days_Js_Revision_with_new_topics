750e149ea9e7f82233c54c0a52b544f931cd0323
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-20 07:14:59 +0000

    Display Methods

---
+++ Day 7/Display_Methods.js
@@ -0,0 +1,8 @@
+let date = new Date();
+console.log(date.getFullYear()); // 2025
+console.log(date.getMonth()); // 3 (April)
+console.log(date.getDay()); // 6 (Saturday)
+
+// Set new year
+date.setFullYear(2026);
+console.log(date.toDateString()); // Sun Apr 19 2026
---
+++ Day 7/New_Dates_MilliSeconds.js
@@ -0,0 +1,6 @@
+let date = new Date(1620000000000); // Milliseconds since Unix Epoch
+console.log(date.toLocaleString()); // Approx May 3, 2021
+
+// Current time ke milliseconds
+let now = Date.now(); // Current milliseconds
+console.log(new Date(now).toLocaleString()); // Current date/time

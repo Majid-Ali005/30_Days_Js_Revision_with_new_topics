ba455c9f09f519dc72366de189d985531cb492ce
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-21 10:12:12 +0000

    Two example

---
+++ Day 8/JavScript Booleans/Another_Simple_Example.js
@@ -0,0 +1,11 @@
+// Assigning Booleans
+const isAdult = true;
+const isMinor = false;
+
+console.log(isAdult); // true
+console.log(isMinor); // false
+
+// Boolean from comparison
+const age = 20;
+const canVote = age >= 18;
+console.log(canVote); // true
---
+++ Day 8/JavScript Booleans/Booleans_Practical_Example_with_ifelse.js
@@ -0,0 +1,11 @@
+// Basic Boolean usage
+const isSunny = true;
+const isRaining = false;
+
+if (isSunny) {
+  console.log("Wear sunglasses!"); // Output: Wear sunglasses!
+} else if (isRaining) {
+  console.log("Take an umbrella!");
+} else {
+  console.log("Enjoy the weather!");
+}

da5f424ab6ff80fe266814723008193b7f6743bf
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-21 08:51:42 +0000

    tho aap iss trha se area of circle ko find kr sakty hy.

---
+++ Day 8/Math_dot_PI.js
@@ -0,0 +1,5 @@
+
+// tho aap iss trha se area of circle ko find kr sakty hy.
+const radius = 5;
+const area = Math.PI * radius * radius;
+console.log(`Area of circle: ${area}`); // Output: Area of circle: 78.53981633974483

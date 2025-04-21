c62f0c173c2e0af208f6577e61187e9dc07e4427
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-21 10:13:41 +0000

    voting app

---
+++ Day 8/JavScript Booleans/Use_In_Voting_Application.js
@@ -0,0 +1,5 @@
+const userAge = 16;
+const isEligible = userAge >= 18;
+console.log(`Can vote: ${isEligible}`); // Can vote: false
+
+// aap iss ko or if,else ki condition k sath or b improve kr sakty ho etc.

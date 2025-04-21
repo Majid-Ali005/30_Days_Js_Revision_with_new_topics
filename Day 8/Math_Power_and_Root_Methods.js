25c6f360a3b449b351f135fa192a28497484dd52
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-21 09:05:13 +0000

    Power and Root Methods

---
+++ Day 8/Math_Power_and_Root_Methods.js
@@ -0,0 +1,11 @@
+
+// Power root methods in javaScript
+const a = 16;
+const b = 5;
+const c = 8;
+const d = 4;
+
+console.log("the square root is" , Math.sqrt(a)); // ye square root nikalne k liye hota hy. output: 4 q k 4 jo 4 za 16 hota hy etc. you know about this.
+console.log("the cube root is" , Math.cbrt(b)); //output: the cube root is 1.709975946676697
+console.log(Math.pow(c,2)); // 8 8 za aap k pass 64 hota hy. tho pow me aap ko do cheeze deni parthi hy etc. output: 64
+console.log(Math.exp(d)); // output: 54.598150033144236

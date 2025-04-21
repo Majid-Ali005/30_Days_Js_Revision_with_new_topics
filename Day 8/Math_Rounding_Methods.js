0dbf80196cd6ea61227a8d23e6effc7476a29019
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-21 08:56:29 +0000

    Rounding Methods Matlab aap iss trha se math k andar different ways se rounding kr sakty ho.

---
+++ Day 8/Math_Rounding_Methods.js
@@ -0,0 +1,11 @@
+
+// tho aap iss trha se math k method ko use kr sakty hy easily
+const a = 4.6;
+const b = 4.1;
+const c = 4.9;
+const d = 4.9;
+
+console.log(Math.round(a)); // 5
+console.log(Math.ceil(b));  // 5
+console.log(Math.floor(c)); // 4
+console.log(Math.trunc(d)); // 4

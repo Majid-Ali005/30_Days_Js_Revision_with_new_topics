ea5f6425ab13be3026178662d9fc4cc8ed7e406b
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-22 07:17:14 +0000

    if else statement in js

--- Day 9/If_Else_Statements.js
+++ Day 9/If_Else_Statements.js
@@ -1,27 +1,25 @@
-
-// simple comparison example
-let age = 18;
-console.log(age >= 18); // True
-console.log(age > 18); // False
 /**
- * ye aap k comparison pr depend krtha hy k aap konsa kiss trha se condition laga rhay ho.
- * jaise first me true iss wja se a rha hy q k aap ne >= equal b sath me lagaya hy jiss k hisab se condition sahi ho rhi hy.
- * or phir niche False wale me aap ne sirf > greater than lagaya hy iss wja se false a rha hy jiss se hamare pass condition false hojati hy etc.
- * tho flow ko control krthy waqt inn cheezo ka khayal rakho.
+ * aap k pass yaha pr agr isLoginIn true ho tho phri welcome to the dashbaord ayega.
+ * warna agr ye false ho tho phir pleas login in. ayega simple.
+ * aap ko (kuch banao) aap ko iss bracket k andar kuch likna partha hy or phir {jo show krna ho} or phir iss curly brackets jiss cheez ko print krwana ho oss ko likhna hy etc.
  */
 
-// Another If Else Example
-
-let isAdult = age >= 18 && age <= 65; // true
-console.log(isAdult);
-// aap iss trha se aik line me b conditions ko likh sakty hy etc.
-
-// Another Only If Condition Statement
-let marks = 75;
-if (marks >= 60) {
-  console.log("You passed the exam!");
+let isLoggedIn = true;
+if (isLoggedIn) {
+  console.log("Welcome to the dashboard!");
+} else {
+  console.log("Please log in.");
 }
-// Output: You passed the exam! q k condition sahi ho rhi hy.
-// Note: agr aap k pass condition match nhi ho rhi hy tho phir aap k pass kuch b print nhi hoga
-// or agr match hota hy tho phir aap k pass you passed the examp! print hojayega.
-// or condition match na hone pr kuch iss waja se print nhi ho rha hy q k aap ne else ki condition nhi lagaye hy.
+// Output: Please log in.
+
+
+// Real Life Example
+let temperature = 25;
+if (temperature > 30) {
+  console.log("Wear light clothes.");
+} else if (temperature > 20) {
+  console.log("Wear a jacket.");
+} else {
+  console.log("Wear warm clothes.");
+}
+// Output: Wear a jacket.

909ce329d3b8e93e835e5c2aabdf0cf090fd5aaf
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-24 09:55:10 +0000

    converting numbers to strings in js

---
+++ Day 11/Js Type Conversion/converting_numbers_to_strings.js
@@ -0,0 +1,11 @@
+// Using String()
+let num = 123.45;
+console.log(String(num)); // "123.45"
+
+// Using toString()
+console.log(num.toString()); // "123.45"
+console.log((42).toString()); // "42"
+
+// Using concatenation
+let str = num + ""; // Implicit conversion
+console.log(str); // "123.45"

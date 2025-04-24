a6f8be1b253e586775317d70a2e1c44cd9fbef58
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-24 09:39:28 +0000

    or yaha pr hamare pass sets or oss k methods cover hojatay hy.

--- Day 11/Js_Sets_Methods.js
+++ Day 11/Js_Sets_Methods.js
@@ -12,3 +12,39 @@ console.log(fruits); // Set(2) {"apple", "banana"}
 let colors = new Set(["red", "blue", "green"]);
 colors.delete("blue");
 console.log(colors); // Set(2) {"red", "green"}
+
+// js has value method
+let numbers = new Set([1, 2, 3]);
+console.log(numbers.has(2)); // true
+console.log(numbers.has(4)); // false
+
+// js clear method
+let animals = new Set(["cat", "dog"]);
+animals.clear();
+console.log(animals); // Set(0) {}
+
+// js search method
+
+let cities = new Set(["Delhi", "Mumbai", "Bangalore"]);
+console.log(cities.size); // 3
+
+// sets are objects
+
+let mySet = new Set();
+console.log(typeof mySet); // "object"
+console.log(mySet instanceof Set); // true
+
+
+// the new method
+
+// From Array
+let arrSet = new Set([1, 2, 2, 3]);
+console.log(arrSet); // Set(3) {1, 2, 3}
+
+// From String
+let strSet = new Set("hello");
+console.log(strSet); // Set(4) {"h", "e", "l", "o"}
+
+// Empty Set
+let emptySet = new Set();
+console.log(emptySet); // Set(0) {}

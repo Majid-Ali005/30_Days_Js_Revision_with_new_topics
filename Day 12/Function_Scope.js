00d5f38f1677d63c0671cf27434c5b89d1070069
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-25 13:41:34 +0000

    Function Scope

---
+++ Day 12/Function_Scope.js
@@ -0,0 +1,6 @@
+function greet() {
+    var functionVar = "Hello from function";
+    console.log(functionVar); // Output: Hello from function
+  }
+  greet();
+  console.log(functionVar); // Error: functionVar is not defined

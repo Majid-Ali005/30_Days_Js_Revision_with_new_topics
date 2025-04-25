a349e2404134502da5b95fafbb2a5a9f0dca0bfb
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-25 13:40:11 +0000

    local scope

---
+++ Day 12/Local_Scope.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    let localVar = "I'm local";
+    console.log(localVar); // Output: I'm local
+  }
+  myFunction();
+  console.log(localVar); // Error: localVar is not defined

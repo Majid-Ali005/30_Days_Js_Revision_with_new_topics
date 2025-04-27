696e3c8d030e84fcc4bd22820ecc79471b9220aa
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-04-27 17:47:48 +0000

    variables names

---
+++ Day 14/Js Style Guide/Variables_Name.js
@@ -0,0 +1,26 @@
+// Bad: Unclear names
+let x = 100;
+let y = x * 0.1;
+
+// Good: Descriptive names
+let price = 100;
+let tax = price * 0.1;
+
+console.log("Tax:", tax); // Outputs 10
+
+
+// Another Example
+
+let isActive = true;
+let hasPermission = false;
+
+// Spaces Around Operators
+
+// Bad: No spaces
+let sum=a+b*c;
+
+// Good: Spaces around operators
+let sum1 = a + b * c;
+
+let a = 5, b = 3, c = 2;
+console.log(sum1); // Outputs 11

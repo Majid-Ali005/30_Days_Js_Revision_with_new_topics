2d88816d387b2dc9d87a5d3ffa09e132b2542263
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-05-02 09:49:00 +0000

    tho yaha pr aap k pass ye hoga k aik second k baad aap k pass Timout jo hy wo print hojaega magr hamare program rokhega nhi first print hoga start and end and then one second complete hone k baad Timout print hoga or yehi aap k pass async programming hoti hy etc.

---
+++ Js Async & Await Day 19/setTimeOut.js
@@ -0,0 +1,7 @@
+console.log("start");
+setTimeout(()=> console.log("Timout"),1000);
+console.log("End");
+/**
+ * tho yaha pr aap k pass ye hoga k aik second k baad aap k pass Timout jo hy wo print hojaega magr hamare program rokhega nhi first print hoga start and end and then one second complete hone k baad Timout print hoga or yehi aap k pass async programming hoti hy etc.
+ * jo k aap k program ko rukhti hy or website ko creash hone se bachati hy etc.
+ */

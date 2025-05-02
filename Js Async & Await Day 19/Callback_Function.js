b215b77ccb6b6b7790ea855c801f2f0018a62532
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-05-02 09:50:58 +0000

    Callback function

---
+++ Js Async & Await Day 19/Callback_Function.js
@@ -0,0 +1,7 @@
+function fetchData(callback) {
+    setTimeout(() => {
+      callback("Data fetched!");
+    }, 1000);
+  }
+  
+  fetchData((data) => console.log(data)); // Output: Data fetched! (1s baad)
